593d667ae9edf611bbc65d68f27c471a8630cedf
HecHerEsp <djwaste@gmail.com>
2026-04-06 19:17:35 +0200

    Actividad 1

---
+++ EventEaseApp/Shared/EventModel.cs
@@ -0,0 +1,9 @@
+namespace EventEaseApp.Shared
+{
+    public class EventModel
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime Fecha { get; set; }
+        public string Ubicacion { get; set; } = string.Empty;
+    }
+}
